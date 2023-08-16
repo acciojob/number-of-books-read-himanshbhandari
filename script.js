3e46e6c6da01943a4512b055c051d367876ce2c8
Himanshu Bhandari <127023539+himanshbhandari@users.noreply.github.com>
2023-08-16 12:53:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,19 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+
+	let count=0;
+
+	for(let i in library)
+		{
+			if(library[i]==true)
+			{
+				count++;
+			}
+			
+		}
+	return count;
+	
 };
 
 // Do not change the code below
